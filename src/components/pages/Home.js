dfb84a6386281d843db8b121d9f991530b54482f
SoWadi <sonia.odh7@gmail.com>
2023-10-15 16:08:32 +0200

    1510

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,12 +1,12 @@
 import React from "react";
-import './animatedSquare.css';
+import "./animatedSquare.css";
 
 import { Link } from "react-router-dom";
 import VMC from "./inc/Vmc";
 import ServiceCommercial from "../images/Slider2.jpg";
 import GestionAdministrative from "../images/Gestion_administrative.jpg";
 import KPIs from "../images/KPIs.jpg";
-import FormationCo from "../images/Formation.jpg"
+import FormationCo from "../images/Formation.jpg";
 import PhotoHome from "../images/AboutFoto2.png";
 
 function Home() {
@@ -16,54 +16,56 @@ function Home() {
 
       {/*      <div className="divAnimation"></div> */}
 
-      <div class="areaHome" >
-            <ul class="circles">
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-                    <li></li>
-            </ul>
+      <div class="areaHome">
+        <ul class="circles">
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+          <li></li>
+        </ul>
 
-      <section className="section my-5 divAnimation text-white">
-        <div className="container">
-          <div className="row">
-            <div className="col-md-12 pb-5 my-auto text-center d-flex flex-column">
-              <h2 className="main-heading">
-                {" "}
-                Linda Tadjer
-                <hr className="w-25 mx-auto" />
-              </h2>
-              <h1>
-                Office manager & Commerciale indépendante
-                {/* Simplifier vos opérations commerciales. */}
-              </h1>
-              <div className="underline mx-auto"></div>
-              <p className=" p-4">Seul on va plus vite, ensemble on va plus loin.</p>
-              {/*            <Link to="/about" className="btn btn-success w-25 shadow my-5">
+        <section className="section my-5 divAnimation text-white">
+          <div className="container">
+            <div className="row">
+              <div className="col-md-12 pb-5 my-auto text-center d-flex flex-column">
+                <h2 className="main-heading">
+                  {" "}
+                  Linda Tadjer
+                  <hr className="w-25 mx-auto" />
+                </h2>
+                <h1>
+                  Office manager & Commerciale indépendante
+                  {/* Simplifier vos opérations commerciales. */}
+                </h1>
+                <div className="underline mx-auto"></div>
+                <p className=" p-4">
+                  Seul on va plus vite, ensemble on va plus loin.
+                </p>
+                {/*            <Link to="/about" className="btn btn-success w-25 shadow my-5">
                 {" "}
                 Read more
               </Link>{" "} */}
-              <a
-                className="my-auto mx-auto py-2 px-4 my-5 enSavoirPlusBtn"
-                href="#more-services"
-              >
-                {" "}
-                En savoir plus
-              </a>{" "}
-            </div>
-            {/* FOTO ABOUT/HOME */}
-            {/*           <div className="col-md-6 my-5">
+                <a
+                  className="my-auto mx-auto py-2 px-4 my-5 enSavoirPlusBtn"
+                  href="#more-services"
+                >
+                  {" "}
+                  En savoir plus
+                </a>{" "}
+              </div>
+              {/* FOTO ABOUT/HOME */}
+              {/*           <div className="col-md-6 my-5">
             <img className="w-100" src={PhotoHome} />
           </div> */}
+            </div>
           </div>
-        </div>
-      </section>
+        </section>
       </div>
       {/* Our Vision, mission and value*/}
       {/* <VMC /> */}
@@ -87,39 +89,43 @@ function Home() {
                     alt="Services"
                   />
 
-                    <div className="card-body">
-                      <h6>Accompagnement sur la stratégie commerciale</h6>
-                      <div className="underline"></div>
-                      <p>
-                      Optimisez vos résultats avec notre accompagnement sur la stratégie commerciale, pour une croissance efficace de votre entreprise.
-                      </p>
-                    </div>
-                    <Link to="/strategie-commerciale" className="btn btn-link text-start">
-                      En savoir plus{" "}
-                    </Link>
-          
+                  <div className="card-body">
+                    <h6>Accompagnement sur la stratégie commerciale</h6>
+                    <div className="underline"></div>
+                    <p>
+                      Je vous accompagne sur la
+                      stratégie commerciale, pour une croissance efficace de
+                      votre entreprise.
+                    </p>
+                  </div>
+                  <Link
+                    to="/strategie-commerciale"
+                    className="btn btn-link text-start"
+                  >
+                    En savoir plus{" "}
+                  </Link>
                 </div>
               </div>
               <div className="col-md-4 cardCustom align-self-center zoomCards">
                 <div className="card shadow">
                   <div className="divGestAdm">
-                  <img
-                    src={GestionAdministrative}
-                    className="w-100 border-bottom-c"
-                    alt="Services"
-                  />
- </div>
-                    <div className="card-body">
-                      <h6>Gestion administrative</h6>
-                      <div className="underline"></div>
-                      <p>
-                      Optimisez votre organisation grâce à notre service de gestion administrative sur mesure, pour une gestion plus fluide de votre entreprise.
-                      </p>
-                    </div>
-                    <Link to="/serviceAdm" className="btn btn-link text-start">
+                    <img
+                      src={GestionAdministrative}
+                      className="w-100 border-bottom-c"
+                      alt="Services"
+                    />
+                  </div>
+                  <div className="card-body">
+                    <h6>Gestion administrative</h6>
+                    <div className="underline"></div>
+                    <p>
+                      Optimisez votre organisation grâce à mes services de
+                      gestion administrative sur mesure.
+                    </p>
+                  </div>
+                  <Link to="/serviceAdm" className="btn btn-link text-start">
                     En savoir plus{" "}
-                    </Link>
-           
+                  </Link>
                 </div>
               </div>
               <div className="col-md-4 cardCustom align-self-end zoomCards">
@@ -130,17 +136,17 @@ function Home() {
                     alt="Services"
                   />
 
-                    <div className="card-body">
-                      <h6>Formation commerciale</h6>
-                      <div className="underline"></div>
-                      <p>
-                      Maximisez vos compétences de vente pour augmenter la performance de votre équipe et stimuler la croissance.
-                      </p>
-                    </div>
-                    <Link to="/atelierProsp" className="btn btn-link text-start">
+                  <div className="card-body">
+                    <h6>Formation commerciale</h6>
+                    <div className="underline"></div>
+                    <p>
+                      Maximisez vos compétences de vente pour augmenter la
+                      performance de votre équipe et stimuler la croissance.
+                    </p>
+                  </div>
+                  <Link to="/atelierProsp" className="btn btn-link text-start">
                     En savoir plus{" "}
-                    </Link>
-              
+                  </Link>
                 </div>
               </div>
             </div>
@@ -148,7 +154,6 @@ function Home() {
         </div>
       </section>
     </div>
-    
   );
 }
 
